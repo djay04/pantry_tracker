a39ef3632826834085e784b07679bf9c61bf80eb
djay04 <117534908+djay04@users.noreply.github.com>
2024-08-03 09:44:46 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,13 +9,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCErndJOkmLthWrR0gjVBlBr5bUAlFU_vs",
-  authDomain: "pantry-tracker-829c0.firebaseapp.com",
-  projectId: "pantry-tracker-829c0",
-  storageBucket: "pantry-tracker-829c0.appspot.com",
-  messagingSenderId: "79642362861",
-  appId: "1:79642362861:web:7beb191593f322c1d95de4",
-  measurementId: "G-M58JY8H1Y6"
 };
 
 // Initialize Firebase
